f50e1a9a7e08402aafac4ccf797b9b5f993ed6c0
gagan01manik <gaganmanik4@gmail.com>
2025-06-30 20:26:52 +0530

    Updated project

---
+++ Temperature.java
@@ -0,0 +1,9 @@
+public class Temperature {
+    public static void main(String[] args) {
+        double celsius = 20.0;
+        double farenheit = (celsius * 9/5) + 32;
+        System.out.println(farenheit);
+    }
+
+    
+}
