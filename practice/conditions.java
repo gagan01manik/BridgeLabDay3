ed7fef5fc2040310e2a9b45148a2da7d0aadbaac
gagan01manik <gaganmanik4@gmail.com>
2025-07-01 20:24:07 +0530

    code push

---
+++ practice/conditions.java
@@ -0,0 +1,16 @@
+package practice;
+import java.util.*;
+
+public class conditions {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>18){
+            System.out.println("adult");
+        }
+        else{
+            System.out.println("not adult");
+        }
+        
+    }
+}
