bce097a524a93386857c509f25172cab69f3b83e
gagan01manik <gaganmanik4@gmail.com>
2025-07-01 20:40:33 +0530

    Updated project

---
+++ in,output/input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String text = sc.nextLine();
+        sc.close();
+    }
+    
+}
